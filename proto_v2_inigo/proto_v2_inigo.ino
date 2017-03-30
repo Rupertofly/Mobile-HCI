b811e7c416325dc3b56f6626d43b7cd8718e58f3
Rupertofly <rupertofly@gmail.com>
2017-03-30 13:26:50 +1100

    commenting code and changing variable names

--- proto_v2_inigo/proto_v2_inigo.ino
+++ proto_v2_inigo/proto_v2_inigo.ino
@@ -1,44 +1,52 @@
 /*********************************************************************
 
-   Human Interface Design (HID)
-   Basic Chorded Keyboard
-   with chord delay and auto repeat
-
-   by Scott Mitchell
-   RMIT University
-
    based on Adafruit Bluefruit code.
    MIT license
 
    This code is designed for use with the Adafruit Bluefruit Feather board
 
+   adapted from code by Scott Mitchell
 *********************************************************************/
 
 #include "keyboardSetup.h"
-String KEYBOARD_NAME = "RupertM_Keyboard";
+
+//-----------------------------------------------------------
+
+// Keyboard Name
+String s_keyboard_name = "RupertM_Keyboard";
+
+//Send Character
 String c_send = "-";
+// Pins
+const int i_pin_gLED = 2;
+const int i_pin_rLED = 3;
+const int i_pin_but5_1 = 5;
+const int i_pin_but1_1 = 6;
+const int i_pin_but1_2 = A2;
+const int i_pin_but2_1 = 10;
+const int i_pin_but2_2 = A1;
+const int i_pin_but3_1 = 11;
+const int i_pin_but3_2 = A0;
+const int i_pin_but4_1 = 12;
+const int i_pin_but4_2 = 13;
 
-const int pin_gLED = 2;
-const int pin_rLED = 3;
-const int pin_but5_1 = 5;
-const int pin_but1_1 = 6;
-const int pin_but1_2 = A2;
-const int pin_but2_1 = 10;
-const int pin_but2_2 = A1;
-const int pin_but3_1 = 11;
-const int pin_but3_2 = A0;
-const int pin_but4_1 = 12;
-const int pin_but4_2 = 13;
-byte but_vals[4];
-byte out_val[3];
-byte loop_since = 0;
-byte high_val = 0;
-boolean send_key = false;
-int send_val;
-boolean z_receive = true;
-boolean receiving = false;
 
-const char keyMap[81] = {'0', 'a', 'e', 'i', 0x08, 'n', 'o', 's', ' ', 'u',
+//Bytes
+byte ya_vals[4];
+byte y_loop = 0;
+byte y_high = 0;
+
+// Bools
+boolean b_send = false;
+boolean b_zero = true;
+boolean b_receive = false;
+
+// Send Val
+int i_send;
+
+
+//Key Map
+const char ca_map[81] = {'0', 'a', 'e', 'i', 0x08, 'n', 'o', 's', ' ', 'u',
                          't', 'h', 'r', '?', 'y', 'd', ',', 'm', '.', 'g',
                          'p', 'c', 'f', 0x21, 'l', 'w', 0x0A, 'x', '~', '~',
                          '~', '~', '~', '~', '~', '~', 'v', '~', '~', 'j',
@@ -50,128 +58,108 @@ const char keyMap[81] = {'0', 'a', 'e', 'i', 0x08, 'n', 'o', 's', ' ', 'u',
                         };
 
 
-
+//--------------------------------------------------------
 void setup() {
-  //Serial.begin(9600);
-  setupKeyboard(KEYBOARD_NAME);
-  pinMode(pin_gLED, OUTPUT);
-  pinMode(pin_rLED, OUTPUT);
-  pinMode(pin_but5_1, INPUT_PULLUP);
-  pinMode(pin_but1_1, INPUT_PULLUP);
-  pinMode(pin_but2_1, INPUT_PULLUP);
-  pinMode(pin_but3_1, INPUT_PULLUP);
-  pinMode(pin_but4_1, INPUT_PULLUP);
-  pinMode(pin_but1_2, INPUT_PULLUP);
-  pinMode(pin_but2_2, INPUT_PULLUP);
-  pinMode(pin_but3_2, INPUT_PULLUP);
-  pinMode(pin_but4_2, INPUT_PULLUP);
+  //Setup keyboard
+  setupKeyboard(s_keyboard_name);
+  //setup all the pins
+  pinMode(i_pin_gLED, OUTPUT);
+  pinMode(i_pin_rLED, OUTPUT);
+  pinMode(i_pin_but5_1, INPUT_PULLUP);
+  pinMode(i_pin_but1_1, INPUT_PULLUP);
+  pinMode(i_pin_but2_1, INPUT_PULLUP);
+  pinMode(i_pin_but3_1, INPUT_PULLUP);
+  pinMode(i_pin_but4_1, INPUT_PULLUP);
+  pinMode(i_pin_but1_2, INPUT_PULLUP);
+  pinMode(i_pin_but2_2, INPUT_PULLUP);
+  pinMode(i_pin_but3_2, INPUT_PULLUP);
+  pinMode(i_pin_but4_2, INPUT_PULLUP);
 
 }
+//--------------------------------------------------------
 void loop() {
-  long loop_start = millis();
-  byte buttonState = readButtonState();
+  long l_time = millis(); //start timing loop
+  byte y_state = ready_state(); //get current button state
   delay(50);
-  byte output_b = convert_button(buttonState);
-  if (output_b == 0 && z_receive == false){
-    z_receive = true;
-    loop_since = 0;
+  byte y_output = convert_button(y_state); //convert to 3rd order state
+  if (y_output == 0 && b_zero == false){ // if current state is zero, and zero is yet to be received, mark zero received
+    b_zero = true;
+    y_loop = 0;
   }
-  if (output_b != 0 && z_receive) receiving = true;
-  if (loop_since <5&&receiving){
-    if (key_check(output_b)) {
-      send_key = true;
-      send_val = high_val;
-      high_val = 0;
-      receiving = 0;
+  if (y_output != 0 && b_zero) b_receive = true; //if receiving a character other then zero,
+  if (y_loop <5&&b_receive){ //if looped less then 5 times and still receiving a characeter other then 0
+    if (key_check(y_output)) { //if looped 5 times
+      b_send = true; //tell code to send
+      i_send = y_high; // tell code what to send
+      y_high = 0; //reset values
+      b_receive = 0;
     }
   }
-  if (send_key && z_receive) {
-    //Serial.print(String(send_val) + " ");
-    send_key = false;
-    z_receive = false;
-    if(keyMap[send_val] != '~'){
-      c_send = String(keyMap[send_val]);
-      digitalWrite(pin_gLED, 1);
-      ble.print("AT+BleKeyboard=");
+  if (b_send) {
+    //Serial.print(String(i_send) + " ");
+    b_send = false; //reset send values
+    b_zero = false;
+    if(ca_map[i_send] != '~'){ //if character is not a ~ (placeholder invalid)
+      c_send = String(ca_map[i_send]); //set output character to corresponding character
+      digitalWrite(i_pin_gLED, 1); //flash green to signify send
+      ble.print("AT+BleKeyboard="); //write character to bluetooth
       ble.println(c_send);
 
-      if ( ble.waitForOK() )
+      if ( ble.waitForOK() ) //if send successfull
       {
-        digitalWrite(pin_gLED, 0);
+        digitalWrite(i_pin_gLED, 0); //stop green led
       }  else
       {
-        digitalWrite(pin_gLED, 0);
-        digitalWrite(pin_rLED, 1);
+        digitalWrite(i_pin_gLED, 0); //flash red led and turn of green led
+        digitalWrite(i_pin_rLED, 1);
         delay(50);
-        digitalWrite(pin_rLED, 0);
+        digitalWrite(i_pin_rLED, 0);
       }
     }
-  } else {
-    Serial.print("0 ");
-  }
-
-  boolean b5 = digitalRead(pin_but5_1);
-  //Serial.print(output_b);
-
-  //Serial.print(" ");
-  //Serial.print(!b5);
-  //Serial.print(" ");
-  //Serial.print(String(loop_since) + " ");
-
-
-  //Serial.println(millis() - loop_start);
-
-
-  if (b5 == 0) {
-    digitalWrite(pin_rLED, 1);
-  }
-  else {
-    digitalWrite(pin_rLED, 0);
   }
 }
-byte readButtonState() {
+byte ready_state() { //get binary indication of buttons pressed
   // start with them all off
-  byte bState = B00000000;
+  byte y_state = B00000000;
 
   // turn on the bits if the button is being pressed
-  if (digitalRead(pin_but1_1) == false) bitSet(bState, 0);
-  if (digitalRead(pin_but1_2) == false) bitSet(bState, 1);
-  if (digitalRead(pin_but2_1) == false) bitSet(bState, 2);
-  if (digitalRead(pin_but2_2) == false) bitSet(bState, 3);
-  if (digitalRead(pin_but3_1) == false) bitSet(bState, 4);
-  if (digitalRead(pin_but3_2) == false) bitSet(bState, 5);
-  if (digitalRead(pin_but4_1) == false) bitSet(bState, 6);
-  if (digitalRead(pin_but4_2) == false) bitSet(bState, 7);
+  if (digitalRead(i_pin_but1_1) == false) bitSet(y_state, 0);
+  if (digitalRead(i_pin_but1_2) == false) bitSet(y_state, 1);
+  if (digitalRead(i_pin_but2_1) == false) bitSet(y_state, 2);
+  if (digitalRead(i_pin_but2_2) == false) bitSet(y_state, 3);
+  if (digitalRead(i_pin_but3_1) == false) bitSet(y_state, 4);
+  if (digitalRead(i_pin_but3_2) == false) bitSet(y_state, 5);
+  if (digitalRead(i_pin_but4_1) == false) bitSet(y_state, 6);
+  if (digitalRead(i_pin_but4_2) == false) bitSet(y_state, 7);
 
 
-  return (bState);
+  return (y_state);
 }
 
-int convert_button(byte b) {
-  byte out_v[4];
+int convert_button(byte _y_b) { //convert binary into a number index number
   for (int i = 0; i < 4; i++) {
-    int pos = i * 2;
-    byte exb;
-    bitWrite(exb, 0, bitRead(b, pos));
-    bitWrite(exb, 1, bitRead(b, pos + 1));
-    if (exb == 3) exb = 2;
-    but_vals[i] = exb;
+    int i_pos = i * 2;
+    byte y_exb;
+    bitWrite(exb, 0, bitRead(_y_b, i_pos));
+    bitWrite(exb, 1, bitRead(_y_b, i_pos + 1));
+    if (exb == 3) y_exb = 2;
+    ya_vals[i] = y_exb;
   }
-  int but_int_val = (but_vals[0] + (but_vals[1] * 3) + (but_vals[2] * 9) + (but_vals[3] * 27));
+  int but_int_val = (ya_vals[0] + (ya_vals[1] * 3) + (ya_vals[2] * 9) + (ya_vals[3] * 27));
   return (but_int_val);
 }
 
-boolean key_check(byte key){
-  if (key!=0){
-    if (key > high_val){
-      high_val = key;
+boolean key_check(byte _y_key){ //check whether current value is higher then previous values, and increment loop count
+  if (_y_key!=0){
+    if (key > y_high){
+      y_high = _y_key;
     }
   }
-  if(loop_since >= 4){
+  if(y_loop >= 4){
     return true;
-    //loop_since=0;
+    //y_loop=0;
   } else {
-    loop_since++;
+    y_loop++;
     return false;
   }
 }
